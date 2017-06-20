01b6d3fe2d795c075c0b6d82446cfa8a6f6daa55
dabaichi <valbendan@hotmail.com>
2017-06-20 14:56:42 +0800

    :)

--- relay_test.py
+++ relay_test.py
@@ -22,21 +22,21 @@ def generate_fake_data(points):  # type: (int) -> list(dict)
     ]
 
 
-def do_influx_request(n_points, host):  # type: (int, str) -> None
+def do_influx_request(n_points, host, database):  # type: (int, str, str) -> None
     conn = InfluxDBClient(host=host)
     conn.switch_database('p2')
     points = generate_fake_data(n_points)
-    conn.write_points(points, time_precision='u')
+    conn.write_points(points, time_precision='u', database=database)
 
 
-def thread_run_request(points, seconds, host):  # type: (int, int, str) -> None
+def thread_run_request(points, seconds, host, database):  # type: (int, int, str, str) -> None
     end_time = time.time() + seconds
     request_count = 0  # type: int
     while time.time() < end_time:
         # do request
-        do_influx_request(points, host)
+        do_influx_request(points, host, database)
         request_count += 1
-    print('request %d in %d seconds!' % (request_count, seconds))
+    print('request %d in %d seconds with %d points!' % (request_count, seconds, points))
 
 
 @click.command()
@@ -44,14 +44,15 @@ def thread_run_request(points, seconds, host):  # type: (int, int, str) -> None
 @click.option('--seconds', default=10, help='持续运行多少时间')
 @click.option('--threads', default=4, help='使用多少个线程')
 @click.option('--host', default='172.20.1.30', help='InfluxDB-relay 的域名或者IP地址')
-def main(points, seconds, threads, host):  # type: (int, int, int, str) -> None
+@click.option('--database', default='test', help='写入到哪个数据库')
+def main(points, seconds, threads, host, database):  # type: (int, int, int, str, str) -> None
     """
     简单的向 InfluxDB-relay 写入数据的工具
     """
     running_threads = []  # type: list(threading.Thread)
 
     for i in range(threads):
-        t = threading.Thread(target=lambda: thread_run_request(points, seconds, host))
+        t = threading.Thread(target=lambda: thread_run_request(points, seconds, host, database))
         t.start()
         running_threads.append(t)
 
